f9529147c81e2186dd6377168e17fb50d66e4aa2
Julien Ripouteau <julien@ripouteau.com>
2021-05-07 23:15:30 +0200

    Added shadow and border radius to candles

--- src/components/primitives/candle.js
+++ src/components/primitives/candle.js
@@ -10,8 +10,22 @@ export default class CandleExt {
         this.draw(data)
     }
 
+    roundRect(ctx, x, y, w, h, r) {
+        if (w < 2 * r) r = w / 2
+        if (h < 2 * r) r = h / 2
+        ctx.beginPath()
+        ctx.moveTo(x + r, y)
+        ctx.arcTo(x + w, y, x + w, y + h, r)
+        ctx.arcTo(x + w, y + h, x, y + h, r)
+        ctx.arcTo(x, y + h, x, y, r)
+        ctx.arcTo(x, y, x + w, y, r)
+        ctx.closePath()
+        return ctx
+    }
+
     draw(data) {
         const green = data.raw[4] >= data.raw[1]
+
         const body_color = green ?
             this.style.colorCandleUp :
             this.style.colorCandleDw
@@ -37,15 +51,26 @@ export default class CandleExt {
         if (data.w > 1.5) {
 
             this.ctx.fillStyle = body_color
-            // TODO: Move common calculations to layout.js
-            let s = green ? 1 : -1
-            this.ctx.fillRect(
-                Math.floor(data.x - hw -1),
-                data.c,
-                Math.floor(hw * 2 + 1),
-                s * Math.max(h, max_h),
-            )
+            if (this.self.sett.shadow) {
+                let sett = this.self.sett
+                this.ctx.shadowColor = sett.shadowColor
+                this.ctx.shadowBlur = sett.shadowBlur
+                this.ctx.shadowOffsetX = sett.shadowOffsetX
+                this.ctx.shadowOffsetY = sett.shadowOffsetY
+            }
 
+            // TODO: Move common calculations to layout.js
+            let candleH = Math.max(h, max_h)
+            
+            const yPos = green ? data.c : data.c - candleH
+            this.roundRect(
+                this.ctx,
+                Math.floor(data.x - hw -1),
+                yPos,
+                Math.floor(hw * 2 + 1),
+                candleH,
+                this.self.sett.roundRadius || 0
+            ).fill()
         } else {
 
             this.ctx.strokeStyle = body_color
